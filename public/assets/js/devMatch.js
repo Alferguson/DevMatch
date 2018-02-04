bfed6fa08a92417fc175c4591751a331313e1c77
alferguson <alferguson@berkeley.edu>
2018-02-03 21:42:16 -0800

    more front end javascript

--- public/assets/js/devMatch.js
+++ public/assets/js/devMatch.js
@@ -61,20 +61,21 @@ $(document).ready(function() {
 
   $("#submit").on("click", function() {
     // QUESTION: What does $.get do? What are the parameters it is expecting?
-    axios.post("/api/", {
-      pageAnsArr
-      console.log(data);
-      if (data) {
-        $("#stats").show();
-        $("#name").text(data.name);
-        $("#role").text(data.role);
-        $("#age").text(data.age);
-        $("#force-points").text(data.forcePoints);
-      }
-      else {
-        $("#name").text("The force is not strong with this one. Your character was not found.");
-        $("#stats").hide();
-      }
-    });
+    axios.post("/api/", pageAnsArr)
+      .then(function(data) {
+        console.log(data);
+        if (data) {
+          $("#stats").show();
+          $("#name").text(data.name);
+          $("#role").text(data.role);
+          $("#age").text(data.age);
+          $("#force-points").text(data.forcePoints);
+        }
+        else {
+          $("#name").text("The force is not strong with this one. Your character was not found.");
+          $("#stats").hide();
+        }
+
+      })
   });
 }) 
